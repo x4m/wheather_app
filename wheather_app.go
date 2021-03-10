425da67f065d1b1ae20dcfb7ddfcb0ab3c852674
Andrey Borodin <amborodin@acm.org>
2021-03-10 18:38:27 +0500

    Demo write to DB

--- wheather_app.go
+++ wheather_app.go
@@ -1,7 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"github.com/jackc/pgx"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	conn, done := getConnection()
+	if done {
+		return
+	}
+
+	for {
+		if conn != nil {
+			_, err := conn.Exec(context.Background(), "insert into wheather values (now(), 'Yekaterinburg', random()*100 - 40)")
+			if err != nil {
+				fmt.Println(err)
+				if conn.IsClosed() {
+					conn = nil
+				}
+			} else {
+				fmt.Println("Wheather was written")
+			}
+		}
+
+		if conn == nil {
+			conn, done = getConnection()
+			if !done {
+				fmt.Println("No connection to DB")
+			}
+		}
+
+		time.Sleep(time.Second / 2)
+	}
+
+	/*result := conn.QueryRow(context.Background(), "select pg_is_in_recovery()");
+	var inRecovery bool
+	err = result.Scan(&inRecovery)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("In recovery: ", inRecovery)*/
+}
+
+func getConnection() (*pgx.Conn, bool) {
+	conn, err := pgx.Connect(context.Background(),
+		"host=rc1c-men71f9ys74qk05u.mdb.yandexcloud.net,rc1b-gss4elrqataonjh7.mdb.yandexcloud.net "+
+			"port=6432 "+
+			"user=user1 "+
+			"password=12345678 "+
+			"database=db1 "+
+			"target_session_attrs=read-write")
+	if err != nil {
+		fmt.Println(err)
+		return nil, true
+	}
+	return conn, false
 }
